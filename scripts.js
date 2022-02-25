29679a7cd18164b46c5a455f7704f29f7809b348
AnnaGaskell <annalilygaskell@gmail.com>
2022-02-25 14:50:54 -0500

    final functions and buttons

--- scripts.js
+++ scripts.js
@@ -1,64 +1,132 @@
-/*DONE*/   //1.contain functions for all of the basic math operators
-//creating functions for add subtract multiply divide
+//Defaults
+let firstOperand = ''
+let secondOperand = ''
+let currentOperation = null
+let resetOutput = false
 
-function add(x,y) {
-	return x + y
-};
 
-function subtract(x,y) {
-	return x - y
-};
+//Buttons
 
-function multiply (x,y) {
-    return x * y
-};
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButtons = document.getElementById('equalsBtn');
+const deleteButton = document.getElementById('deleteBtn');
+const clearButton = document.getElementById('clearBtn');
+const decButton = document.getElementById('decimalBtn')
+const prevInput = document.getElementById('previousInput');
+const currentInput = document.getElementById('currentInput');
 
-function divide(x,y) {
-    return x / y
-};
-/*DONE*/  //2. create function operate that takes an operator and 2 numbers 
-// and then calls one of the above functions on the numbers
 
-function operate (x, operation, y) {
-    if (operation === '+') return add(x, y);
-    if (operation === '-') return subtract(x, y);
-    if (operation === '*') return multiply(x, y);
-    if (operation === '/') return y === 0 ? "Can't divide by 0!" : divide(x, y);
+//and their Functionality
+
+numberButtons.forEach((button) => 
+    button.addEventListener('click', () => appendNum(button.textContent))
+)
+
+operationButtons.forEach((button) => 
+    button.addEventListener('click', () => chooseOperation(button.textContent))
+)
+
+equalsButtons.addEventListener('click', evaluate)
+deleteButton.addEventListener('click', remove)
+clearButton.addEventListener('click', clear)
+decButton.addEventListener('click', decimal)
+
+
+//Functions
+
+function remove(){
+    currentInput.textContent = currentInput.textContent.toString().slice(0,-1)
 }
 
-console.log(operate(10,"*", 4));
+function clear() {
+    currentInput.textContent = '0'
+    prevInput.textContent = ''
+    firstOperand = ''
+    secondOperand= ''
+    currentOperation = null
+}
 
-/*DONE*/  //3. HTML and CSS
+function decimal(){
+    if (resetOutput) resetScreen()
+    if (currentInput.textContent === '')
+        currentInput.textContent = '0'
+    if (currentInput.textContent.includes('.')) return
+        currentInput.textContent += '.'
+}
 
-//4. Create the functions that populate the display when you click the number buttons
-// you should be storing the ‘display value’ 
 
-//5.Make the calculator work! You’ll need to store the first number that is input into 
-//the calculator when a user presses an operator, and also save which operation has been chosen 
-//and then operate() on them when the user presses the “=” key.
+function appendNum(number){
+   if (currentInput.textContent === '0' || resetOutput)
+        resetScreen()
+    currentInput.textContent += number
+}
 
-    //a.You should already have the code that can populate the display, so once operate() 
-    //has been called, update the display with the ‘solution’ to the operation.
+function resetScreen(){
+    currentInput.textContent = ''
+    resetOutput = false
+}
 
-    //b. You need to figure out how to store all the values and 
-    //call the operate function with them.
+function evaluate(){
+    if (currentOperation === null || resetOutput) return
+    if (currentOperation === '÷' && currentInput.textContent === '0'){
+        alert("I think not")
+        return
+    }
 
-//Gotchas: watch out for and fix these bugs if they show up in your code:
-    //a. Users should be able to string together several operations and 
-    //get the right answer, with each pair of numbers being evaluated at a time. 
-    //For example, 12 + 7 - 5 * 3 = should yield 42.
+    secondOperand = currentInput.textContent
+    currentInput.textContent = rounded(
+        operate(currentOperation, firstOperand, secondOperand)
+    )
 
-    //b. Your calculator should not evaluate more than a single pair of numbers at a time.
-    // Example evaluate the first pair of numbers (12 + 7), second, display the result of 
-    // that calculation (19), and finally, use that result (19) as the first number in your new calculation, 
-    //along with the next operator (-).
+    prevInput.textContent = `${firstOperand} ${currentOperation} ${secondOperand} = `
+    currentOperation = null
+}
 
-    //c. You should round answers with long decimals so that they don’t overflow the screen.
 
-    //d.Pressing = before entering all of the numbers or an operator could cause problems!
+function chooseOperation(operator) {
+    if (currentOperation !== null) evaluate()
+    firstOperand = currentInput.textContent
+    prevInput.textContent = `${firstOperand} ${operator}`
+    resetOutput = true
+}
 
-    //e. Pressing “clear” should wipe out any existing data.. make sure the user is 
-    //really starting fresh after pressing “clear”
+function rounded(number){
+   return Math.round(number*100)/100
+}
 
-    //f.Display a snarky error message if the user tries to divide by 0… 
-    //don’t let it crash your calculator!
+
+function add(a,b) {
+	return a + b
+};
+
+function subtract(a,b) {
+	return a - b
+};
+
+function multiply (a,b) {
+    return a * b
+};
+
+function divide(a,b) {
+    return a / b
+};
+
+
+function operate (operator, a, b) {
+    a = Number(a)
+    b = Number(b)
+    switch (operator) {
+      case '+':
+        return add(a, b)
+      case '−':
+        return substract(a, b)
+      case '×':
+        return multiply(a, b)
+      case '÷':
+        if (b === 0) return null
+        else return divide(a, b)
+      default:
+        return null
+}
+}
